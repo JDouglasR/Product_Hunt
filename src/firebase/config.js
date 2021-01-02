f4c9b66b30ea7ccc98c6e4c6a03119550ba5e2e3
Justin Roose <justin.d.roose@gmail.com>
2021-01-02 15:30:34 -0500

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdsJ9rWttbtRRWsamjAtfOoskDppTIQvI",
+  authDomain: "product-hunt-e7d46.firebaseapp.com",
+  projectId: "product-hunt-e7d46",
+  storageBucket: "product-hunt-e7d46.appspot.com",
+  messagingSenderId: "903957356130",
+  appId: "1:903957356130:web:abb065f1f4c53a00b7fb3b",
+};
+
+export default firebaseConfig;
